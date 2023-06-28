cc0a330fff0cb03bb85e4cd2c29b5c67d4f631a2
John Noble <j.nble92@gmail.com>
2023-06-28 01:56:01 -0400

    changing frontend config for AWS v2

--- vue/noble/src/main.js
+++ vue/noble/src/main.js
@@ -12,7 +12,7 @@ Vue.config.productionTip = false
 
 
 // axios.defaults.baseURL = "http://localhost:8080/api/v1";
-axios.defaults.baseURL = "http://3.224.247.73:8080/api/v1/";
+axios.defaults.baseURL = "https://3.224.247.73:8080/api/v1/";
 
 
 new Vue({
